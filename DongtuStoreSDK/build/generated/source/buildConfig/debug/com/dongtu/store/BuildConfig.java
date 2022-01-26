388dd1922a0a79c552fd8e2e35f93e22856d55be
windq <shihaowind@163.com>
2022-01-26 10:37:21 +0800

    Initial commit

---
+++ DongtuStoreSDK/build/generated/source/buildConfig/debug/com/dongtu/store/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dongtu.store;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dongtu.store";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ DongtuStoreSDK/build/generated/source/buildConfig/release/com/dongtu/store/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dongtu.store;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.dongtu.store";
+  public static final String BUILD_TYPE = "release";
+}
